68a3eec175b035b32dd6eebd223097050c5d510b
kim <smkim000721@naver.com>
2025-04-27 19:07:04 +0900

    메소드 설명 주석 추가

--- student/src/main/java/com/example/student/StudentService.java
+++ student/src/main/java/com/example/student/StudentService.java
@@ -11,23 +11,23 @@ import org.springframework.transaction.annotation.Transactional;
 public class StudentService {
 	@Autowired
 	StudentMapper studentMapper;
-	
+	// 전체출력
 	public List<Student> getAll(){
 		return studentMapper.selectAll();
 	}
-	
+	// 한명 출력
 	public Student getOneByName(String name) {
 		return studentMapper.selectOneByName(name);
 	}
-	
+	// 추가
 	public int add(Student student) {
 		return studentMapper.insert(student);
 	}
-	
+	// 수정
 	public int modify(Student student) {
 		return studentMapper.update(student);
 	}
-	
+	// 삭제
 	public int remove(String name) {
 		return studentMapper.delete(name);
 	}
