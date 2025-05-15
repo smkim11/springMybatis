fcf11f07d0077518b44d17164dd03c1ca8005fcf
kim <smkim000721@naver.com>
2025-05-15 10:21:10 +0900

    filter사용하기위해 @ServletComponentScan 추가

--- mbboard/src/main/java/com/example/mbboard/MbboardApplication.java
+++ mbboard/src/main/java/com/example/mbboard/MbboardApplication.java
@@ -2,8 +2,10 @@ package com.example.mbboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication // controller, service, mapper... 자동 스캔 후 bean등록
+@ServletComponentScan // webservlet, listener 자동스캔 후 bean등록
 public class MbboardApplication {
 
 	public static void main(String[] args) {
